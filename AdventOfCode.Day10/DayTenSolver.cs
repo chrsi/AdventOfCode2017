8f8f484961d6cb7583898bf62127a9771f52d913
Christian Siber <christian.siber@dataformers.at>
2017-12-10 12:58:12 +0100

    Implement Day 10

---
+++ AdventOfCode.Day10/DayTenSolver.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using MoreLinq;
+
+namespace AdventOfCode.Day10
+{
+    public class DayTenSolver
+    {
+        private int skipSize = 0;
+        private int currIndex = 0;
+
+        public int SolvePart1(string file)
+        {
+            int[] length;
+            int[] arr = new int[256];
+            for (int i = 0; i < 255; ++i)
+            {
+                arr[i] = i;
+            }
+
+            using (StreamReader reader = new StreamReader(file))
+            {
+                length = reader.ReadLine().Split(",").Select(item => int.Parse(item)).ToArray();
+            }
+
+            return CalcHash(arr, length);
+        }
+
+        public string SolvePart2(string file)
+        {
+            string length;
+            using (StreamReader reader = new StreamReader(file))
+            {
+                length = reader.ReadLine();
+            }
+            return CalcHashPart2(length);
+        }
+
+        public string CalcHashPart2(string input)
+        {
+            int[] length = input.Select(ch => (int)ch).ToArray();
+            int[] arr = new int[256];
+            for (int i = 0; i < 256; ++i)
+            {
+                arr[i] = i;
+            }
+            length = length.Concat(new[] { 17, 31, 73, 47, 23 }).ToArray();
+
+            for (int i = 0; i < 64; ++i)
+            {
+                HashRound(arr, length);
+            }
+
+            var densedHash = DenseHash(arr);
+            return string.Join("", densedHash.Select(d => d.ToString("X2"))).ToLower();
+        }
+
+        private IEnumerable<int> DenseHash(int[] arr)
+        {
+            return arr.Batch(16).Select(b => b.Aggregate((acc, val) => (acc ^ val)));
+        }
+
+        public int CalcHash(int[] arr, int[] lengths)
+        {
+            HashRound(arr, lengths);
+            return arr[0] * arr[1];
+        }
+
+        public void HashRound(int[] arr, int[] lengths)
+        {
+
+            foreach (var length in lengths)
+            {
+                var affectedSubArr = GetFromArr(arr, currIndex, length);
+                Array.Reverse(affectedSubArr);
+                ReplacePart(arr, affectedSubArr, currIndex);
+
+                skipSize++;
+                currIndex = (currIndex + length + skipSize - 1) % arr.Length;
+            }
+        }
+
+        private void ReplacePart(int[] arr, int[] affectedSubArr, int currIndex)
+        {
+            for (int i = 0; i < affectedSubArr.Length; ++i)
+            {
+                arr[(currIndex + i) % arr.Length] = affectedSubArr[i];
+            }
+        }
+
+        private int[] GetFromArr(int[] arr, int currIndex, int length)
+        {
+            int[] partial = new int[length];
+            for (int i = 0; i < length; ++i)
+            {
+                partial[i] = arr[(currIndex + i) % arr.Length];
+            }
+            return partial;
+        }
+    }
+}
---
+++ AdventOfCode.UnitTests/Day10Tests.cs
@@ -0,0 +1,59 @@
+﻿using AdventOfCode.Day10;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode.UnitTests
+{
+    [TestClass]
+    public class Day10Tests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var solver = new DayTenSolver();
+            int[] arr = { 0, 1, 2, 3, 4 };
+            int[] lengthes = { 3, 4, 1, 5 };
+
+            Assert.AreEqual(12, solver.CalcHash(arr, lengthes));
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            var solver = new DayTenSolver();
+            Assert.AreEqual(4480, solver.SolvePart1("../../../Resources/input-10.txt"));
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            var solver = new DayTenSolver();
+            Assert.AreEqual("a2582a3a0e66e6e86e3812dcb672a272", solver.CalcHashPart2(""));
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            var solver = new DayTenSolver();
+            Assert.AreEqual("33efeb34ea91902bb2f59c9920caa6cd", solver.CalcHashPart2("AoC 2017"));
+        }
+        [TestMethod]
+        public void Test5()
+        {
+            var solver = new DayTenSolver();
+            Assert.AreEqual("3efbe78a8d82f29979031a4aa0b16a9d", solver.CalcHashPart2("1,2,3"));
+        }
+        [TestMethod]
+        public void Test6()
+        {
+            var solver = new DayTenSolver();
+            Assert.AreEqual("63960835bcdc130f0b66d7ff4f6a5a8e", solver.CalcHashPart2("1,2,4"));
+        }
+        
+        [TestMethod]
+        public void Test7()
+        {
+            var solver = new DayTenSolver();
+            Assert.AreEqual("c500ffe015c83b60fad2e4b7d59dabc4", solver.SolvePart2("../../../Resources/input-10.txt"));
+        }
+    }
+}
